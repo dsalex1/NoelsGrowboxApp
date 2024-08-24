fbf20e9399f47542f7e584154d033bfdbaa790a9
dsalex1 <dsalex1@ymail.com>
2024-08-24 02:49:34 +0200

    fixed timezone conversion

--- app/Models/DataLog.php
+++ app/Models/DataLog.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 class DataLog extends Model
 {
     use HasFactory;
 
     protected $fillable = ['type', 'value', 'unit'];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
